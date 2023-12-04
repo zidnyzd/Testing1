699f2c016c977d026051cf975099dbdca048e5e2
Muhammad Zidny Ilhami <76208286+zidnyzd@users.noreply.github.com>
2023-12-05 05:32:18 +0700

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/AutoFTbot/suprame/main/permission/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/zidnyzd/sscc/main/permission/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
